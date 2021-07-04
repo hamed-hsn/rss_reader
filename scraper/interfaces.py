0ef12a6cf32011bcbec3f575e66d2df063af356e
senator <hamedhsn93@gmail.com>
2021-07-04 21:50:02 +0430

    some changes

--- scraper/interfaces.py
+++ scraper/interfaces.py
@@ -5,12 +5,12 @@ class InterfaceScraper(metaclass=ABCMeta):
 
     __METHODS__ = ('scrape')
 
-    @classmethod
-    def __subclasshook__(cls, subclass):
-        for method in cls.__METHODS__:
-            if not (hasattr(subclass, method) and callable(getattr(subclass, method))):
-                return False
-        return True
+    # @classmethod
+    # def __subclasshook__(cls, subclass):
+    #     for method in cls.__METHODS__:
+    #         if not (hasattr(subclass, method) and callable(getattr(subclass, method))):
+    #             return False
+    #     return True
 
     @abstractmethod
     def scrape(self):
