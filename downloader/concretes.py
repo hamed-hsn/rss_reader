5899c87316f3b6cfdae4325082d3e4fa17cff141
senator <hamedhsn93@gmail.com>
2021-07-04 20:31:22 +0430

    add concreate module

---
+++ downloader/concretes.py
@@ -0,0 +1,49 @@
+from _typeshed import NoneType
+from downloader.abstract import AbstractDownloader
+import requests
+from typing import Dict, Tuple, Union
+from requests_html import HTMLSession
+
+
+class CommonDownloader(AbstractDownloader):
+    
+    def __init__(self, url):
+        super().__init__(url)    
+
+    def download(self):
+        res = requests.get(self.url)
+        self._status_code = res.status_code
+        self._text = res.text
+    
+    
+class FeaturedDownloader(AbstractDownloader):
+
+    def __init__(self, url, timeout:Tuple(int, int)=(5,15), proxies:Union[NoneType, Dict]=None):
+        super().__init__(url)
+        self._timeout = timeout
+        self._proxies = proxies
+        self._text = self.DOWNLOADER_NOT_SET_TEXT
+        self._status_code = self.DOWNLOADER_NOT_SET_STATUS_CODE
+
+    def download(self):
+        res = requests.get(self.url, timeout=self._timeout, proxies=self._proxies)
+        self._status_code = res.status_code
+        self._text = res.text
+    
+
+class JavaScriptRenderableDownloader(AbstractDownloader):
+
+    DOWNLOADER_NOT_SET_STATUS_CODE = -1
+    DOWNLOADER_NOT_SET_TEXT = 'BAD'
+
+    def __init__(self, url):
+        super().__init__(url)        
+        self._text = self.DOWNLOADER_NOT_SET_TEXT
+        self._status_code = self.DOWNLOADER_NOT_SET_STATUS_CODE
+
+    def download(self):
+        session = HTMLSession()
+        res = session.get(self.url)
+        res.html.render()
+        self._text = res.html.html
+        self._status_code = res.status_code
